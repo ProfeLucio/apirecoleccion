619b3a7ba41376e2821c9c972833b950e255f308
Gonzalo Lucio <gonzalo.lucio@correounivalle.edu.co>
2026-06-17 11:18:23 -0500

    Vehiculo all

--- app/Http/Controllers/Api/VehiculoController.php
+++ app/Http/Controllers/Api/VehiculoController.php
@@ -31,6 +31,7 @@ class VehiculoController extends Controller
     public function index(Request $request)
     {
         $request->validate(['perfil_id' => 'required|uuid|exists:perfiles,id']);
+        //Return all vehicles associated with the given perfil_id
         return Vehiculo::where('perfil_id', $request->query('perfil_id'))->get();
     }
 
